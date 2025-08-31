ae7ef5739b32ed55fb6548896253b4524aaf8ef0
Roman <roman@macbook.local>
2025-08-31 11:13:11 +0300

    Fix logick for reducer and call methods

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,9 @@
 // Type action
 export const SET_USER_DATA = 'SET_USER_DATA';
+export const SET_REG_TOKEN = 'SET_REG_TOKEN';
+export const SET_USER = 'SET_USER';
 
 // Action creators
-export const setUserData = ( user ) => ({
-  type: SET_USER_DATA,
-  payload: user,
-});
+export const setUserData = ( user ) => ({ type: SET_USER_DATA, payload: user, });
+export const setRegTokenAction = ( token ) => ({ type: SET_REG_TOKEN, payload: token });
+export const setUserAction = ( user ) => ({ type: SET_USER, payload: user });
