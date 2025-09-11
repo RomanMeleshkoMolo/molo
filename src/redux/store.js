8d99f16e987fab2133f4fd9fe4904ef3f0486e4b
Roman <roman@macbook.local>
2025-09-11 14:05:11 +0300

    Added new Page LoginUserBirthday

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,19 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import reducer from './reducer';
 
-const store = createStore(reducer);
+// В whitelist кладем ключи верхнего уровня из initialState,
+// которые хотим сохранять между перезапусками.
+const persistConfig = {
+  key: 'user', // ключ в AsyncStorage
+  storage: AsyncStorage,
+  whitelist: ['userData', 'regToken', 'userName'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
 
 export default store;
